f07547e97135a23968d7e49763fba78f8e1a7dd0
slumpbuster <djsellers17@gmail.com>
2022-03-20 13:09:49 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# Shopping Cart
+
+## Description 
+This is a simple Shopping Cart using react to create and manage a todo list. A list of items sold are presented as buttons with the correlating stock next to it.
+
+## Purpose 
+This was done as an assignment in the MIT course - Full Stack Development with Mern
+
+---------
+
+## Technologies Used 
+- HTML
+- CSS
+- Javascript
+
+---------
+
+## Installation 
+- Clone this repository to your local machine
+- Open a command line on your computer and run the command cd path-to-project-root (this should be the actual directory where the repository is located on your local machine)
+- Within the same command-line window, run npm install http-server which will allow you to fire up a web server to access the paage
+- Once the command completes successfully, run http-server -c-1
+- Open your browser of choice and browse to http://127.0.0.1:8080/
+
+## How to Run 
+- When the page is loaded in your browser, you will notice default entries of items sold
+- To add an item to your shopping cart: Click on the button of an entry. If there is a 0 next to the entry, the item will not be added to your cart. If there is not a zero next to the entry, the item will be added to your cart and the inventory of that item will be reduced by one.
+
+---------
+
+## Files 
+- **/index.html** - Start-up file to be opened by browse 
+- **/shop.js** - Contains the React code to build the stores items, the code to manage the inventory, the code to manage the shopping cart, and Render ReactDOM
+
+---------
+
+## Contributing 
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+## License
+[The MIT License (MIT)](https://github.com/slumpbuster/Formik/blob/main/LICENSE)
---
+++ shop.jsx
@@ -0,0 +1,69 @@
+// provide a button and use onClick to move 1 item into the Shopping Cart
+// use React.useState to keep track of items in the Cart.
+// list out the Cart items in another column
+function ShoppingCart({ availableItems }) {
+  const { Button } = ReactBootstrap;
+  const [stock, setStock] = React.useState(availableItems);
+  const [cart, setCart] = React.useState([]);
+
+  const moveToCart = (e) => {
+    let [product, numInStock] = e.target.innerHTML.split(":");
+    if (numInStock <= 0) return;
+    let item = stock.filter((item) => item.product == product);
+    let newStock = stock.map((item) => {
+      if (item.product == product) {
+        item.inStock--;
+      }
+      return item;
+    });
+    setStock([...newStock]);
+    setCart([...cart, ...item]);
+  };
+
+  // No need to update code beyond this point
+  const availableItemsButtons = availableItems.map((item, index) => {
+    return (
+      <Button id={item.product} key={index} onClick={moveToCart}>
+        {item.product}:{item.inStock}
+      </Button>
+    );
+  });
+
+  // Note: React requires a single Parent element, that's why we use <>
+  return (
+    <>
+      <ul key="stock" style={{ listStyleType: 'none' }}>
+        {availableItemsButtons}
+      </ul>
+      <h1>Shopping Cart</h1>
+      <Cart cartitems={cart}> Cart Items</Cart>
+    </>
+  );
+}
+
+function Cart({ cartitems }) {
+  const { Button } = ReactBootstrap;
+  console.log('rendering Cart');
+  const availableItemsButtons = cartitems.map((item, index) => {
+    return (
+      <Button id={item.product} key={index}>
+        {item.product}
+      </Button>
+    );
+  });
+  return (
+    <ul id="cart-items" style={{ listStyleType: 'none' }} key="cart">
+      {availableItemsButtons}
+    </ul>
+  );
+}
+
+const availableItems = [
+  { product: 'Jacket', inStock: 2 },
+  { product: 'Pants', inStock: 3 },
+  { product: 'Scarf', inStock: 0 },
+  { product: 'Pajamas', inStock: 3 },
+  { product: 'Shirt', inStock: 1 },
+];
+
+ReactDOM.render(<ShoppingCart availableItems={availableItems} />, document.getElementById('root'));
